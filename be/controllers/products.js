e97ae8e50e883332d8dbe68df0880af7cd6cffbd
Tungndph <150453700+Tungndph@users.noreply.github.com>
2024-07-18 22:31:30 +0700

    crud-product

--- be/controllers/products.js
+++ be/controllers/products.js
@@ -12,21 +12,56 @@ class ProductController {
       res.status(400).json({ message: error.message });
     }
   }
-  async getProductDetail() {
+  async getProductDetail(req, res) {
     try {
-    } catch (error) {}
+      const product = await Product.findById(req.params.id);
+      if (!product) {
+        return res.status(404).json({ message: "Product NotFound" });
+      }
+      res
+        .status(200)
+        .json({ message: "Get Product Details Done", data: product });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
   }
-  async createProduct() {
+  async createProduct(req, res) {
     try {
-    } catch (error) {}
+      const product = await Product.create(req.body);
+      res
+        .status(201)
+        .json({ message: "Create Product Successfully!!!", data: product });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
   }
-  async updateProduct() {
+  async updateProduct(req, res) {
     try {
-    } catch (error) {}
+      const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      });
+      if (!product) {
+        return res.status(404).json({
+          message: " Product Not Found",
+        });
+      }
+      res
+        .status(200)
+        .json({ message: "Update Product Successfully!!!", data: product });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
   }
-  async deleteProduct() {
+  async deleteProduct(req, res) {
     try {
-    } catch (error) {}
+      const product = await Product.findByIdAndDelete(req.params.id);
+      if (!product) {
+        return res.status(404).json({ message: "Product Not Found" });
+      }
+      res.status(200).json({ message: "Delete Product Successfully!!!" });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
   }
 }
 
