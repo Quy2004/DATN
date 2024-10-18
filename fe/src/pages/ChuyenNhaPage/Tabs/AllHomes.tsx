9e792f719f46cb73322c409fbc44969d78bedff6
quytv3804 <Quykinas1412@gmail.com>
2024-10-18 15:58:34 +0700

    chuyennha

--- fe/src/pages/ChuyenNhaPage/Tabs/AllHomes.tsx
+++ fe/src/pages/ChuyenNhaPage/Tabs/AllHomes.tsx
@@ -162,9 +162,9 @@ const AllHomes: React.FC = () => {
                                     đang bàn tán xôn xao về SIGNATURE -...</p>
                             </div>
                         </div>
-                        <div>
+                        <div className="">
                             <Link to="coffeeholic" onClick={scrollToTop}>
-                                <h3 className="border-2 w-1/3 mx-auto my-8 py-2 px-4 rounded-lg bg-white font-semibold text-[17px]">
+                                <h3 className="border-2 w-1/3 mx-auto my-12 py-2 px-4 rounded-lg bg-white font-semibold text-[17px]">
                                     Tìm hiểu thêm
                                 </h3>
                             </Link>
@@ -266,7 +266,7 @@ const AllHomes: React.FC = () => {
                     </div>
                     <div>
                         <Link to="teaholic" onClick={scrollToTop}>
-                            <h3 className="border-2 w-1/3 mx-auto my-8 py-2 px-4 rounded-lg bg-white font-semibold text-[17px]">
+                            <h3 className="border-2 w-1/3 mx-auto my-12 py-2 px-4 rounded-lg bg-white font-semibold text-[17px]">
                                 Tìm hiểu thêm
                             </h3>
                         </Link>
@@ -362,7 +362,7 @@ const AllHomes: React.FC = () => {
                             </div>
                         </div>
                         {/*  */}
-                        <div className="grid grid-cols-3 *:rounded-[10px] py-3">
+                        <div className="grid grid-cols-3 *:rounded-[10px] py-4">
                             <div className="col-span-1">
                                 <ul className="">
                                     <li className="">
@@ -391,7 +391,7 @@ const AllHomes: React.FC = () => {
                         </div>
                         <div>
                             <Link to="#">
-                                <h3 className="border-2 w-1/3 mx-auto my-8 py-2 px-4 rounded-lg bg-white font-semibold text-[17px]">
+                                <h3 className="border-2 w-1/3 mx-auto my-12  py-2 px-4 rounded-lg bg-white font-semibold text-[17px]">
                                     Tìm hiểu thêm
                                 </h3>
                             </Link>
