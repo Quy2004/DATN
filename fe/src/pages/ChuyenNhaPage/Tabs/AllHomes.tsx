62d96d513140f5978ca657dd906be278f3bd2aea
quytv3804 <Quykinas1412@gmail.com>
2024-10-18 15:56:08 +0700

    chuyennha

--- fe/src/pages/ChuyenNhaPage/Tabs/AllHomes.tsx
+++ fe/src/pages/ChuyenNhaPage/Tabs/AllHomes.tsx
@@ -73,7 +73,7 @@ const AllHomes: React.FC = () => {
                             </li>
                         </ul>
                     </div>
-                    <h1>Left</h1>
+
                 </div>
                 <div className="cow ml-8 col-span-3">
                     <div>
@@ -164,7 +164,7 @@ const AllHomes: React.FC = () => {
                         </div>
                         <div>
                             <Link to="coffeeholic" onClick={scrollToTop}>
-                                <h3 className="border-2 w-1/3 mx-auto my-8 py-2 rounded-lg bg-white font-semibold text-[17px]">
+                                <h3 className="border-2 w-1/3 mx-auto my-8 py-2 px-4 rounded-lg bg-white font-semibold text-[17px]">
                                     Tìm hiểu thêm
                                 </h3>
                             </Link>
@@ -266,7 +266,7 @@ const AllHomes: React.FC = () => {
                     </div>
                     <div>
                         <Link to="teaholic" onClick={scrollToTop}>
-                            <h3 className="border-2 w-1/3 mx-auto my-8 py-2 rounded-lg bg-white font-semibold text-[17px]">
+                            <h3 className="border-2 w-1/3 mx-auto my-8 py-2 px-4 rounded-lg bg-white font-semibold text-[17px]">
                                 Tìm hiểu thêm
                             </h3>
                         </Link>
@@ -298,7 +298,6 @@ const AllHomes: React.FC = () => {
                             </li>
                         </ul>
                     </div>
-                    <h1>Left</h1>
                 </div>
                 <div className="cow ml-8 col-span-3">
                     <div>
@@ -392,7 +391,7 @@ const AllHomes: React.FC = () => {
                         </div>
                         <div>
                             <Link to="#">
-                                <h3 className="border-2 w-1/3 mx-auto my-8 py-2 rounded-lg bg-white font-semibold text-[17px]">
+                                <h3 className="border-2 w-1/3 mx-auto my-8 py-2 px-4 rounded-lg bg-white font-semibold text-[17px]">
                                     Tìm hiểu thêm
                                 </h3>
                             </Link>
