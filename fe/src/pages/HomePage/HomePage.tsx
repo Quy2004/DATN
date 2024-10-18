3f008ae6ab350a7599a280710e608b9d6d4e7d7a
quytv3804 <Quykinas1412@gmail.com>
2024-10-18 15:36:24 +0700

    callAPI_Product

--- fe/src/pages/HomePage/HomePage.tsx
+++ fe/src/pages/HomePage/HomePage.tsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom";
 import Homes from "../../components/Homes";
 import "./HomePage.css"
 import React, { useState, useEffect } from 'react';
+import instance from "../../services/api";
+import { Product } from "../../types/product";
 
 const HomePage: React.FC = () => {
+  // slideShow
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const images: string[] = [
     "/src/assets/images/banner/Banner_1.webp",
@@ -19,82 +22,63 @@ const HomePage: React.FC = () => {
 
     return () => clearInterval(interval);
   }, [currentIndex, images.length]);
-
+  //showProduct
+  const [products, setProducts] = useState([]); // State lưu danh sách sản phẩm
+  // const [loading, setLoading] = useState(true); // State kiểm soát việc hiển thị trạng thái loading
+  const dataLocal = localStorage.getItem('W209_USER_INFO');
+  const [dataLocalStorage, setDataLocalStorage] = useState('')
+  useEffect(() => {
+    if (dataLocal) {
+      const newData = JSON.parse(dataLocal)
+      setDataLocalStorage(newData);
+    }
+  }, [])
+  const fetchProducts = async () => {
+    try {
+      const { data } = await instance.get("/products"); // Gọi API từ backend
+      setProducts(data.data); // Lưu dữ liệu sản phẩm vào state
+      // setLoading(false); // Tắt trạng thái loading
+    } catch (error) {
+      console.error("Lỗi khi lấy sản phẩm:", error);
+      // setLoading(false); // Tắt trạng thái loading trong trường hợp lỗi
+    }
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  // Định dạng số
+  const formatPrice = (price: number) => {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  };
   return (
     <>
       <div >
         <img src={images[currentIndex]} alt="" className="w-max" />
         <div className='containerAll mx-auto home'>
           <h1 className='font-semibold text-3xl p-3'>Sản Phẩm Hot</h1>
-          <div className='row grid grid-cols-2 gap-7 text-left'>
-            <div className="cow_left">
+          <div className='row grid grid-cols-4 gap-7 text-left h-[330px]'>
+            <div className="cow_left col-span-2">
               <img src="/src/assets/images/banner/banner4.jpg" alt="" className="mb-10" />
             </div>
-            <div className="cow_right flex gap-5 ">
-              <div className="item ">
-                <div className="product_img">
-                  <Link to='#'>
-                    <img src="/src/assets/images/CF_sua_da.webp" alt="" />
-                  </Link>
-                </div>
-                <Link to="#">
-                  <h3 className="">Cà Phê Sữa Đá</h3>
-                </Link>
-                <p>200$ </p>
-              </div>
-              <div className="item ">
-                <div className="product_img">
-                  <img src="/src/assets/images/CF_sua_da.webp" alt="" />
-                </div>
-                <Link to="#">
-                  <h3>Cà Phê Sữa Đá</h3>
-                </Link>
-                <p>200$ </p>
-              </div>
+            <div className="cow_right flex gap-5 col-span-2">
+              {
+                products.map((product: Product) => (
+                  <div className="item ">
+                    <div className="product_img">
+                      <Link to='#'>
+                        <img src={`${product.image}`} alt="" />
+                      </Link>
+                    </div>
+                    <Link to="#">
+                      <h3 className="">{product.name}</h3>
+                    </Link>
+                    <p>{formatPrice(product.price)} VNĐ </p>
+                  </div>
+                ))
+              }
             </div>
           </div>
-          <div className="row grid grid-cols-2 gap-7 text-left">
-            <div className=" flex gap-5">
-              <div className="item ">
-                <div className="product_img">
-                  <img src="/src/assets/images/CF_sua_da.webp" alt="" />
-                </div>
-                <Link to="#">
-                  <h3>Cà Phê Sữa Đá</h3>
-                </Link>
-                <p>200$ </p>
-              </div>
-              <div className="item ">
-                <div className="product_img">
-                  <img src="/src/assets/images/CF_sua_da.webp" alt="" />
-                </div>
-                <Link to="#">
-                  <h3>Cà Phê Sữa Đá</h3>
-                </Link>
-                <p>200$ </p>
-              </div>
-            </div>
-            <div className=" flex gap-5">
-              <div className="item ">
-                <div className="product_img">
-                  <img src="/src/assets/images/CF_sua_da.webp" alt="" />
-                </div>
-                <Link to="#">
-                  <h3>Cà Phê Sữa Đá</h3>
-                </Link>
-                <p>200$ </p>
-              </div>
-              <div className="item ">
-                <div className="product_img">
-                  <img src="/src/assets/images/CF_sua_da.webp" alt="" />
-                </div>
-                <Link to="#">
-                  <h3>Cà Phê Sữa Đá</h3>
-                </Link>
-                <p>200$ </p>
-              </div>
-            </div>
-          </div>
+          {/*  */}
           <div className="banner-mid">
             <img src="/src/assets/images/banner/Banner_TX.jpg" alt="" />
           </div>
