4b4478f9897195dfa34568b9651fe8a36e781a87
quytv3804 <Quykinas1412@gmail.com>
2024-10-18 15:44:39 +0700

    edit footer

--- fe/src/components/Footer.tsx
+++ fe/src/components/Footer.tsx
@@ -28,9 +28,8 @@ const Footer: React.FC = () => {
                                 <h4 className="font-bold text-base flex"><div className="mt-1 mr-2"><FaPhoneAlt /></div>Đặt hàng: 1800 1800</h4>
                                 <h4 className="font-bold text-base flex"><div className="mt-1 mr-2"><FaMapMarkerAlt /></div>Liên hệ</h4>
                                 <p className="text-xs w-40">
-                                    Tầng 3-4 Hub Building
-                                    195/10E Điện Biên Phủ, P.15,
-                                    Q.Bình Thạnh, TP.Hồ Chí Minh
+                                    Cổng số 1, Tòa nhà FPT Polytechnic, 13 phố Trịnh Văn Bô,
+                                    Km12 Cầu Diễn, Phường Phúc Diễn, Quận Bắc Từ Liêm, Hà Nội
                                 </p>
                             </div>
                             <div className="cow">
@@ -67,9 +66,9 @@ const Footer: React.FC = () => {
                                 <div className="col-lg-9 col-md-9 col-sm-9 col-xs-12 footer_col">
                                     <ul className="footer_itemlists text-xs">
                                         <li>Công ty cổ phần thương mại dịch vụ Trà Cà Phê VN</li>
-                                        <li>Mã số DN: 0312867172 do sở kế hoạch và đầu tư tp. HCM cấp ngày 23/07/2014. Người đại diện: NGÔ NGUYÊN KHA</li>
-                                        <li>Địa chỉ: 86-88 Cao Thắng, phường 04, quận 3, tp Hồ Chí Minh &nbsp; Điện thoại: (028) 7107 8079 &nbsp; Email: hi@thecoffeehouse.vn</li>
-                                        <li>© 2014-2022 Công ty cổ phần thương mại dịch vụ Trà Cà Phê VN mọi quyền bảo lưu</li>
+                                        <li>Mã số DN: 0886847253 do sở kế hoạch và đầu tư tp. Hà Nội cấp ngày 04/09/2022. Người đại diện: TRẦN VĂN QUÝ</li>
+                                        <li>Địa chỉ: Tòa nhà FPT Polytechnic, 13 phố Trịnh Văn Bô, phường Phương Canh, quận Nam Từ Liêm, TP Hà Nội &nbsp; Điện thoại: (037) 286 3006 &nbsp; Email: quytv3804@gmail.com</li>
+                                        <li>© 2022-2024 Công ty cổ phần thương mại dịch vụ Trà Cà Phê VN mọi quyền bảo lưu</li>
                                     </ul>
                                 </div>
                             </div>
