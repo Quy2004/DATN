9492cbad7c196dd42128e2c8cff53ba8cd4dae37
giangFE <giangnvph37638@gmail.com>
2024-10-19 19:57:44 +0700

    router-cart

--- be/controllers/Cart.js
+++ be/controllers/Cart.js
@@ -11,6 +11,10 @@ export const addtoCart = async (req, res) => {
       return res.status(400).json({ message: "User ID and products are required." });
     }
 
+    // Kiểm tra nếu giỏ hàng tồn tại
+    let cart = await Cart.findOne({ userId });
+
+  
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Server error", error: error.message });
