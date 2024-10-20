8a18db2ddc66a4b6c52a9e043a85bfcd4786b263
giangFE <giangnvph37638@gmail.com>
2024-10-20 20:20:51 +0700

    cart-models

--- be/models/Cart.js
+++ be/models/Cart.js
@@ -10,7 +10,8 @@ const cartItem = new Schema({
 const cartSchema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: "User" },
     products: [cartItem],
-   
+    totalprice: { type: Number, default: 0 },
+    
 });
 
 export default mongoose.model("carts", cartSchema);
