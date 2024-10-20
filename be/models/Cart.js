c3d731588241533c907560e6736f13066bbf6919
giangFE <giangnvph37638@gmail.com>
2024-10-20 20:19:52 +0700

    cart-models

--- be/models/Cart.js
+++ be/models/Cart.js
@@ -2,6 +2,9 @@ import mongoose from 'mongoose'; // Thêm dòng này
 
 const Schema = mongoose.Schema;
 
-
+const cartItem = new Schema({
+    product: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+    quantity: { type: Number, required: true, min: 1 }
+});
 
 export default mongoose.model("carts", cartSchema);
