1d1e754f746b189164c65b013cf83a7d02cd41aa
giangFE <giangnvph37638@gmail.com>
2024-10-20 20:21:37 +0700

    cart-models

--- be/models/Cart.js
+++ be/models/Cart.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const cartItem = new Schema({
     product: { type: Schema.Types.ObjectId, ref: "Product", required: true },
-   
+    quantity: { type: Number, required: true, min: 1 }
 });
 
 const cartSchema = new Schema({
