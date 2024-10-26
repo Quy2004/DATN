070e79c5dc98be04072c4cf7a0a63116a8add102
baolong101 <nguyenbaolong2712004@gmail.com>
2024-10-26 20:22:40 +0700

    update getById category post

--- be/controllers/categoryPost.js
+++ be/controllers/categoryPost.js
@@ -4,72 +4,98 @@ class CategoryPostController {
 	// hiển thị, lọc, search category post
 	async getAllCategoryPosts(req, res) {
 		try {
-			const {
-				page = 1,
-				limit = 10,
-				search = "",
-				isDeleted,
-			} = req.query;
+			const { page = 1, limit = 10, search = "", isDeleted } = req.query;
 
-            // tạo lọc
-            const query = {
-                isDeleted: isDeleted === "true"? true : false,
-            }
+			// tạo lọc
+			const query = {
+				isDeleted: isDeleted === "true" ? true : false,
+			};
 
-            if(search){
-                query.title= {$regex: search, $options: "i"}
-            }
+			if (search) {
+				query.title = { $regex: search, $options: "i" };
+			}
 
-            // phân trang
-            const pageLimit = parseInt(limit, 10) ||10
-            const currentPage = parseInt(page, 10) || 1
+			// phân trang
+			const pageLimit = parseInt(limit, 10) || 10;
+			const currentPage = parseInt(page, 10) || 1;
 
-            // thực hiện query để lấy danh sách với limit và skip
-            const categoryPosts = await CategoryPost.findOne(query)
-            .limit(pageLimit)
-            .skip((currentPage - 1) * pageLimit)
-            .lean()
+			// thực hiện query để lấy danh sách với limit và skip
+			const categoryPosts = await CategoryPost.findOne(query)
+				.limit(pageLimit)
+				.skip((currentPage - 1) * pageLimit)
+				.lean();
 
-            // tổng số danh mục để tính tổng số trang
-            const totalItems = await CategoryPost.countDocuments(query);
-            res.status(200).json({
-                message: "Lấy danh mục post thành công",
-                data: categoryPosts,
-                pagination: {
-                  totalItems: totalItems,
-                  currentPage: currentPage,
-                  totalPages: Math.ceil(totalItems / pageLimit),
-                },
-              });
-
-
+			// tổng số danh mục để tính tổng số trang
+			const totalItems = await CategoryPost.countDocuments(query);
+			res.status(200).json({
+				message: "Lấy danh mục post thành công",
+				data: categoryPosts,
+				pagination: {
+					totalItems: totalItems,
+					currentPage: currentPage,
+					totalPages: Math.ceil(totalItems / pageLimit),
+				},
+			});
 		} catch (error) {
 			res.status(400).json({ message: error.message });
 		}
 	}
 
-    // Thêm category post mới
-    async createCategoryPost(req, res) {
+	// hiển thị chi tiết 1 danh mục
+	async getCategoryById(req, res) {
+		try {
+			const { id } = req.params;
+			const categoryPost = await CategoryPost.findById(id);
+			if (!categoryPost) {
+				return res.status(404).json({ message: "category post không tìm thấy" });
+			}
+			res.status(200).json({
+				message: "Get category post Details Done",
+				data: categoryPost,
+			});
+		} catch (error) {
+			res.status(400).json({ message: error.message });
+		}
+	}
+
+	// Thêm category post mới
+	async createCategoryPost(req, res) {
+		try {
+			const { title, description, thumbnail, isDeleted } = req.body;
+
+			const categoryPost = await CategoryPost.create({
+				title,
+				description,
+				thumbnail,
+				isDeleted,
+			});
+
+			res.status(201).json({
+				message: "Thêm danh mục bài đăng thành công",
+				data: categoryPost,
+			});
+		} catch (error) {
+			res.status(400).json({ message: error.message });
+		}
+	}
+
+    // sửa 1 danh mục
+    async updateCategoryPost(req, res) {
         try {
-            const { title, description, thumbnail, isDeleted } = req.body;
-            
-            const categoryPost = await CategoryPost.create({
-                title,
-                description,
-                thumbnail,
-                isDeleted
-            });
-    
-            res.status(201).json({
-                message: "Thêm danh mục bài đăng thành công",
-                data: categoryPost,
-            });
+          const categoryPost = await CategoryPost.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+          });
+          if (!categoryPost) {
+            return res.status(404).json({ message: "Category post không tìm thấy" });
+          }
+          res.status(200).json({
+            message: "Update category post Successfully",
+            data: categoryPost,
+          });
         } catch (error) {
-            res.status(400).json({ message: error.message });
+          res.status(error.message).json({ message: error.message });
         }
     }
-    
-
 }
 
 export default CategoryPostController;
--- be/routes/categoryPost.js
+++ be/routes/categoryPost.js
@@ -5,6 +5,8 @@ const categoryPostRouter= express.Router();
 const categoryPost= new CategoryPostController()
 
 categoryPostRouter.get("/", categoryPost.getAllCategoryPosts);
+categoryPostRouter.get("/:id", categoryPost.getCategoryById);
 categoryPostRouter.post("/", categoryPost.createCategoryPost)
+categoryPostRouter.put("/:id", categoryPost.updateCategoryPost)
 
 export default categoryPostRouter
