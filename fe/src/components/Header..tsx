38b1e4f5db82fc4650833bec400d6c75afe794b4
baolong101 <nguyenbaolong2712004@gmail.com>
2024-11-11 22:13:13 +0700

    sửa code

--- fe/src/components/Header..tsx
+++ fe/src/components/Header..tsx
@@ -6,9 +6,13 @@ import instance from "../services/api";
 import { Product } from "../types/product";
 import CartItem from "./CartItem";
 import { useClickOutside } from "./ClickOutSide";
+import toast from "react-hot-toast";
 
 const Header: React.FC = () => {
-	const user = JSON.parse(localStorage.getItem("user") || '');
+	const storedUser = localStorage.getItem("user");
+  const user = storedUser ? JSON.parse(storedUser!) : {};
+
+  
 	const [cart, setCart] = useState<any>([]);
 	const [idCart, setIdCart] = useState<number>()
 	const [isModalOpen, setIsModalOpen] = useState(false);
@@ -83,6 +87,14 @@ const Header: React.FC = () => {
 	const toggleDropdown = () => {
 		setIsDropdownOpen(!isDropdownOpen);
 	};
+
+	const handleLogout = () => {
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+        toast.success("Đã đăng xuất thành công!", { duration: 2000 });
+        navigate("/login");
+        // setSignIn(true); // Cập nhật trạng thái để hiện form đăng nhập
+    };
 
 	return (
 		<>
@@ -224,7 +236,7 @@ const Header: React.FC = () => {
 															</Link>
 														</li>
 														<li>
-															<button className="w-max px-4 py-2 ">
+															<button className="w-max px-4 py-2 " onClick={handleLogout}>
 																<h3 className="font-bold">
 																	Đăng xuất
 																</h3>
