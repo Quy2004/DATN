e708fa674b092180697e7727ab6fbb5b5e2af3d7
Chanhk4 <hoadhph35952@fpt.edu.vn>
2024-11-22 22:51:58 +0700

    Res CoffeePage

--- fe/src/pages/CoffeePage/Coffee.tsx
+++ fe/src/pages/CoffeePage/Coffee.tsx
@@ -22,28 +22,30 @@ const CoffeePage: React.FC = () => {
             return response.data.data;
         },
     });
-    
+
     // Định nghĩa giá
     const formatPrice = (price: number) => {
         return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     };
 
     return (
-        <div className="containerAll mx-auto px-4">
-            <div className="mb-8">
-                <h1 className="pt-10 text-3xl font-semibold mb-3 mt-6">Cà phê</h1>
-                <p className="border-b-orange-400 w-24 border-b-[4px]"></p>
+        <div className="containerAll mx-4 md:px-4 md:mx-auto">
+            <div className="*:mx-auto mb-6 md:mb-8">
+                <h1 className="w-max pt-4 text-xl font-semibold mb-1 mt-6 md:text-3xl md:mb-3 md:pt-10">Cà Phê</h1>
+                <p className=" border-b-orange-400 w-[61px] border-b-[4px]"></p>
             </div>
-            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 text-left mb-12">
+            <div className="flex flex-wrap gap-x-5 gap-y-4 justify-center md:gap-5 text-left mb-4 md:justify-start md:grid-cols-4">
                 {productsData?.map((coffee: Product) => (
                     <div key={coffee._id} className="item">
-                        <Link to={`/detail/${coffee._id}`} className="product_img">
-                            <img src={coffee.image} alt={coffee.name} className="w-full h-auto object-cover"/>
+                        <Link to={`/detail/${coffee._id}`} className="md:product_img">
+                            <img src={coffee.image} alt={coffee.name} className="w-[180px] h-[180px] border-2 rounded-lg md:w-full md:h-auto object-cover" />
                         </Link>
-                        <Link to="#">
-                            <h3 className="text-2xl mt-2">{coffee.name}</h3>
-                        </Link>
-                        <p className="text-lg font-semibold">{formatPrice(coffee.price)} VNĐ</p>
+                        <div className="*:leading-4 ">
+                            <Link to="#">
+                                <h3 className="text-2xl mt-0 md:mt-2">{coffee.name}</h3>
+                            </Link>
+                            <p className="text-sm font-semibold">{formatPrice(coffee.price)} VNĐ</p>
+                        </div>
                     </div>
                 ))}
             </div>
