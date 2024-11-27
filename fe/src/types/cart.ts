b727dc3eba7f08fd811afdf3108e875268ab3628
Tungndph <150453700+Tungndph@users.noreply.github.com>
2024-11-28 00:49:59 +0700

    Thêm chức năng export excel đơn hàng và lọc bài viết theo danh mục

--- fe/src/types/cart.ts
+++ fe/src/types/cart.ts
@@ -1,13 +1,15 @@
 import { Product } from "./product";
 
 export interface CartItem{
+    product_toppings: boolean;
     quantity : number
 }
 export interface Cart{
     _id?: string;
     userId: string;
     products : [Product],
-    totalprice : number
+    totalprice: number
+    
 }
 export interface CartItem {
     product: Product;
