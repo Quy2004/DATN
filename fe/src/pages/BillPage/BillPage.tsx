2183b732a3f0913f650ddc2a1b491ab9a5cbec86
Chanhk4 <hoadhph35952@fpt.edu.vn>
2024-12-07 16:30:00 +0700

    UpdateBillPage, StyleAccountUpdate, StyleForgotPassword, DataDetailPage

--- fe/src/pages/BillPage/BillPage.tsx
+++ fe/src/pages/BillPage/BillPage.tsx
@@ -31,7 +31,8 @@ const BillPage = () => {
         refetchOnWindowFocus: true,  // Tự động fetch khi cửa sổ được focus
         enabled: !!storedUserId,  // Chỉ fetch khi có storedUserId
     });
-    console.log(orders)
+
+    console.log(orders);
     // Effect để tự động cập nhật dữ liệu mỗi 5 giây
     useEffect(() => {
         refetch();
@@ -101,20 +102,43 @@ const BillPage = () => {
     if (isError) return <div>Error loading orders.</div>;
     if (!orders || orders.length === 0) return <div>No orders found.</div>;
 
-    // Tính thành tiền
-
+    // Lấy đơn hàng mới nhất
     const latestOrder = orders[0];
+
+    // Hàm tính tổng cộng
+    const calculateTotalPrice = () => {
+        if (!latestOrder || !Array.isArray(latestOrder.orderDetail_id)) return 0;
+
+        // Tổng tất cả các sản phẩm
+        const totalItemsPrice = latestOrder.orderDetail_id.reduce((total: number, item: any) => {
+            const basePrice = Number(item.sale_price) || 0;
+            const sizePrice = Number(item.product_size?.priceSize) || 0;
+            const toppingsPrice = Array.isArray(item.product_toppings)
+                ? item.product_toppings.reduce((acc: number, topping: any) => acc + (Number(topping?.topping_id?.priceTopping) || 0), 0)
+                : 0;
+            const quantity = Number(item.quantity) || 1;
+
+            const totalItemPrice = (basePrice + sizePrice + toppingsPrice) * quantity;
+            return total + totalItemPrice;
+        }, 0);
+
+        // Trừ đi giảm giá
+        return totalItemsPrice - (latestOrder.discountAmount || 0);
+    };
+
+    // Tổng cộng sau khi tính toán
+    const totalPrice = calculateTotalPrice();
 
     return (
         <div>
             {latestOrder && (
-                <div key={latestOrder._id} className="containerAll mt-16 bg-slate-100 max-w-md mx-auto border p-6 text-sm font-sans md:mt-20 md:mb-4">
+                <div className="containerAll mt-16 bg-slate-100 max-w-md mx-auto border p-6 text-sm font-sans md:mt-20 md:mb-4">
                     <img
                         src="/src/pages/BillPage/Logoremove.png"
                         className="hidden w-24 mb-1 mx-auto md:block"
                         alt="Logo"
                     />
-                    <p className="hidden text-center text-gray-600 md:block">Liên hệ: 0987777777</p>
+                    <p className="hidden text-center text-gray-600 md:block">Liên hệ: 0352537706</p>
                     <h3 className="text-center text-[#ea8205] font-semibold text-lg mt-4 md:font-bold">HÓA ĐƠN THANH TOÁN</h3>
 
                     <div className="mt-4 text-gray-800">
@@ -138,12 +162,11 @@ const BillPage = () => {
                                 }, 0)
                                 : 0;
                             const quantity = Number(item.quantity) || 1;
-                            const discount = Number(item.discount) || 0;
                             // Tính tổng giá cho một đơn vị sản phẩm
                             const totalItemPrice = basePrice + sizePrice + toppingsPrice;
 
                             // Tính giá cuối cùng sau khi nhân số lượng và trừ giảm giá
-                            const finalPrice = (totalItemPrice * quantity) - discount;
+                            const finalPrice = (totalItemPrice * quantity) - latestOrder.discountAmount;
 
                             return (
                                 <div key={index} className="mb-6 bg-[#fff] rounded-lg shadow- p-4">
@@ -151,14 +174,12 @@ const BillPage = () => {
                                         <h4 className="text-base font-semibold text-gray-700">{item.product_id.name}</h4>
                                         <span className="text-sm text-gray-500">x{quantity}</span>
                                     </div>
-
                                     <div className="space-y-2 text-sm text-gray-600">
                                         {/* Giá cơ bản */}
                                         <div className="flex justify-between">
                                             <span>Giá cơ bản:</span>
                                             <span className="font-medium">{basePrice.toLocaleString()} VND</span>
                                         </div>
-
                                         {/* Size và giá size */}
                                         {item.product_size && (
                                             <div className="flex justify-between">
@@ -166,6 +187,7 @@ const BillPage = () => {
                                                 <span className="font-medium">{item.product_size?.priceSize.toLocaleString()} VND</span>
                                             </div>
                                         )}
+
                                         {/* Topping và tổng giá topping */}
                                         {Array.isArray(item.product_toppings) && item.product_toppings.length > 0 && (
                                             <>
@@ -189,18 +211,29 @@ const BillPage = () => {
                                             </>
                                         )}
                                         {/* Đơn giá tổng (trước khi nhân số lượng) */}
-                                        <div className="flex justify-between font-medium">
+                                        <div className="flex justify-between ">
                                             <span>Đơn giá:</span>
-                                            <span>{totalItemPrice.toLocaleString()} VND</span>
+                                            <span className="font-medium">{totalItemPrice.toLocaleString()} VND</span>
                                         </div>
-
                                         {/* Giảm giá nếu có */}
-                                        {discount > 0 && (
+                                        {latestOrder.discountAmount > 0 && (
                                             <div className="flex justify-between text-red-500">
                                                 <span>Giảm giá:</span>
-                                                <span className="font-medium">-{discount.toLocaleString()} VND</span>
+                                                <span className="font-semibold">
+                                                    - {latestOrder.discountAmount.toLocaleString()} VND
+                                                </span>
                                             </div>
                                         )}
+                                        {/* Ghi chú */}
+                                        {latestOrder.note && latestOrder.note.trim() && (
+                                            <div className="flex justify-between mt-3">
+                                                <span className="text-gray-700">Ghi chú:</span>
+                                                <span className="text-gray-600 italic">
+                                                    {latestOrder.note}
+                                                </span>
+                                            </div>
+                                        )}
+
 
                                         {/* Thành tiền cuối cùng */}
                                         <div className="flex justify-between text-[#ea8205] font-semibold pt-2 border-t">
@@ -209,14 +242,15 @@ const BillPage = () => {
                                         </div>
                                     </div>
                                 </div>
+
                             );
                         })}
                     </div>
-
-
                     <div className="mt-4 text-right text-gray-800">
-                        <p className="font-bold">Tổng cộng: <span className="text-xl text-[#ea8205]">{latestOrder.totalPrice.toLocaleString()} VND</span></p>
-                        <p className="italic text-gray-600">({numberToWords(latestOrder.totalPrice)})</p>
+                        <p className="font-bold">
+                            Tổng cộng: <span className="text-xl text-[#ea8205]">{totalPrice.toLocaleString()} VND</span>
+                        </p>
+                        <p className="italic text-gray-600">({numberToWords(totalPrice)})</p>
                     </div>
                     <p className="text-center mt-4 text-gray-800 font-medium">
                         Xin cảm ơn Quý khách! / Thank you!
