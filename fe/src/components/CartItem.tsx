86076bb39bc8ec8d22b1de2a43f7b4cb23097d99
giangFE <giangnvph37638@gmail.com>
2024-12-15 19:59:59 +0700

    update-cart-fe

--- fe/src/components/CartItem.tsx
+++ fe/src/components/CartItem.tsx
@@ -19,7 +19,7 @@ const CartItem: React.FC<{
         setProduct(data.data);
       } catch (error) {
         console.error("Lỗi khi lấy sản phẩm:", error);
-        toast.error("Không thể tải sản phẩm.");
+        
       }
     };
     if (item?._id) {
@@ -64,9 +64,6 @@ const CartItem: React.FC<{
   const basePrice = item?.product?.sale_price || item?.sale_price || 0; // Giá cơ bản của sản phẩm (ưu tiên lấy từ product nếu tồn tại)
   const totalPrice = (basePrice + priceSize + priceTopping) * quantity; // Tính tổng giá
 
-
-
-
   const formatCurrency = (amount: number) => {
     return `${new Intl.NumberFormat("vi-VN", {
       maximumFractionDigits: 0, // Bỏ phần thập phân nếu không cần
@@ -77,9 +74,7 @@ const CartItem: React.FC<{
     <div className="flex items-center border-b-2 pb-2">
       {item && (
         <>
-          {/* Checkbox */}
-
-
+          
           {/* Product Image */}
           <div className="w-1/5">
             <img src={item?.image} alt="Ảnh" className="border rounded-lg mt-2 w-[50px] h-[50px] object-cover" />
@@ -93,35 +88,11 @@ const CartItem: React.FC<{
             </p>
           </div>
 
-
-
-          {/* Quantity */}
           <div >
             <span className="font-semibold">x{quantity}</span>
           </div>
 
-          {/* Delete Button */}
-          {/* <div className="w-1/5 ml-2">
-            <button
-              className="border p-2 rounded-lg bg-gray-200 hover:bg-gray-400"
-              onClick={deleteSelectedItems}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                />
-              </svg>
-            </button>
-          </div> */}
+          
         </>
       )}
     </div>
